01f2e0e8516063f5c62c7123e2aa21491eac19bb
Abhay Kumar Jain <73276018+Abhaykumarjain14@users.noreply.github.com>
2023-06-11 21:54:14 +0530

    Create Takinginput.py

---
+++ Takinginput.py
@@ -0,0 +1,11 @@
+name = input("What is your name? ")
+print("Hello " + name)
+print("Welcome to our cool Python class")
+
+
+#output
+What is your name?  abhay
+
+Hello abhay
+Welcome to our cool Python class
+
